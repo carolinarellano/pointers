27e7603db8465a859a490464d4b412d811c56e15
Carolina Arellano <carolina.arellanov02@gmail.com>
2022-08-27 11:40:32 -0500

    Insertion sort (kinda fail) and reverse  functions

--- app/app.c
+++ app/app.c
@@ -1,7 +1,11 @@
 #include "pointers.h"
 int main(void)
 {
-    int array[] = {4, 5, 9, 1, 23, 60, 8};
-    bubble_sort(array, 7);
+    int array[30] = {4, 5, 9, 1, 23, 60, 8, 2, 81};
+
+    //bubble_sort(array, 9);
+    insertion_sort(array, 9);
+    //reverse(array, 9);
+
     return 0;
 }
